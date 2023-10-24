ec4083e50090f795ee400c0a2e8f15eaa9c83906
ericaostini <ericaostini14@gmail.com>
2023-10-24 16:14:14 +0200

    fixed creation of invited object arrays

--- js/script.js
+++ js/script.js
@@ -1,81 +1,102 @@
 // snack 1
-const vips = [
-    {
-        guestName: 'Brad Pitt',
-        tableName : 'Tavolo Vip',
-        place: 1
+// const vips = [
+//     {
+//         guestName: 'Brad Pitt',
+//         tableName : 'Tavolo Vip',
+//         place: 1
 
-    },
-    {
-        guestName: 'Johnny Depp',
-        tableName : 'Tavolo Vip',
-        place: 2
+//     },
+//     {
+//         guestName: 'Johnny Depp',
+//         tableName : 'Tavolo Vip',
+//         place: 2
 
-    },
-    {
-        guestName: 'Lady Gaga',
-        tableName : 'Tavolo Vip',
-        place: 3
+//     },
+//     {
+//         guestName: 'Lady Gaga',
+//         tableName : 'Tavolo Vip',
+//         place: 3
 
-    },
-    {
-        guestName: 'Cristiano Ronaldo',
-        tableName : 'Tavolo Vip',
-        place: 4
+//     },
+//     {
+//         guestName: 'Cristiano Ronaldo',
+//         tableName : 'Tavolo Vip',
+//         place: 4
 
-    },
-    {
-        guestName: 'Georgina Rodriguez',
-        tableName : 'Tavolo Vip',
-        place: 5
+//     },
+//     {
+//         guestName: 'Georgina Rodriguez',
+//         tableName : 'Tavolo Vip',
+//         place: 5
 
-    },
-    {
-        guestName: 'Chiara Ferragni',
-        tableName : 'Tavolo Vip',
-        place: 6
+//     },
+//     {
+//         guestName: 'Chiara Ferragni',
+//         tableName : 'Tavolo Vip',
+//         place: 6
 
-    },
-    {
-        guestName: 'Fedez',
-        tableName : 'Tavolo Vip',
-        place: 7
+//     },
+//     {
+//         guestName: 'Fedez',
+//         tableName : 'Tavolo Vip',
+//         place: 7
 
-    },
-    {
-        guestName: 'George Cloney',
-        tableName : 'Tavolo Vip',
-        place: 8
+//     },
+//     {
+//         guestName: 'George Cloney',
+//         tableName : 'Tavolo Vip',
+//         place: 8
 
-    },
-    {
-        guestName: 'Amal Clooney',
-        tableName : 'Tavolo Vip',
-        place: 9
+//     },
+//     {
+//         guestName: 'Amal Clooney',
+//         tableName : 'Tavolo Vip',
+//         place: 9
 
-    },
-    {
-        guestName: 'Maneskin',
-        tableName : 'Tavolo Vip',
-        place: 10
+//     },
+//     {
+//         guestName: 'Maneskin',
+//         tableName : 'Tavolo Vip',
+//         place: 10
 
-    }
-]
-
-
-vips.forEach((vip) => displaySegnaPosto(vip)) 
-
-
-function displaySegnaPosto(vip){
-    const lista = document.getElementById("lista")
+//     }
+// ]
+const vips = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+const invitati = [];
+const tavolo = "Tavolo Vip"
+for (let i = 0; i < vips.length; i++){
+    invitati.push(
+        {
+            guestName: vips[i],
+            tableName: tavolo,
+            place: i
+        }
+    )
+}
+console.log(invitati)
+const lista = document.getElementById("lista")
     lista.innerHTML += `
     <ul>
-        <li> Guest Name: ${vip.guestName} </li>
-        <li> Table Name: ${vip.tableName} </li>
-        <li> Place: ${vip.place} </li>
+        <li> Guest Name: ${invitati.guestName} </li>
+        <li> Table Name: ${invitati.tableName} </li>
+        <li> Place: ${invitati.place} </li>
     </ul>
     `
-}
+
+
+// vips.forEach((vip) => displaySegnaPosto(vip)) 
+
+
+// function displaySegnaPosto(vip){
+//     const lista = document.getElementById("lista")
+//     lista.innerHTML += `
+//     <ul>
+//         <li> Guest Name: ${vip.guestName} </li>
+//         <li> Table Name: ${vip.tableName} </li>
+//         <li> Place: ${vip.place} </li>
+//     </ul>
+//     `
+// }
 
 //snack 2
 const students = [
