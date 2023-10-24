75576055adb5551b842b92b917003797ca0b38ac
ericaostini <ericaostini14@gmail.com>
2023-10-24 15:10:49 +0200

    creation students object-arrays

--- js/script.js
+++ js/script.js
@@ -77,5 +77,46 @@ function displaySegnaPosto(vip){
     `
 }
 
+//snack 2
+const students = [
+    {
+        Id: 213,
+        Name: "Marco della Rovere",
+        Grades: 78 
+    },
+    {
+        Id: 110,
+        Name: "Paola Cortellessa",
+        Grades: 96
+    },
+    {
+        Id: 250,
+        Name: "Andrea Mantegna",
+        Grades: 48
+    },
+    {
+        Id: 145,
+        Name: "Gaia Borromini",
+        Grades: 74
+    },
+    {
+        Id: 196,
+        Name: "Luigi Grimaldello",
+        Grades: 68
+    },
+    {
+        Id: 102,
+        Name: "Piero della Francesca",
+        Grades: 50
+    },
+    {
+        Id: 120,
+        Name: "Francesco de Polenta",
+        Grades: 84
+    }
+];
+    
+
+
 
 
