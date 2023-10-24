1e599e5a86e1ecd41d29f178aed0f75d9db9dedc
ericaostini <ericaostini14@gmail.com>
2023-10-24 14:48:08 +0200

    creation object-array vips

--- js/script.js
+++ js/script.js
@@ -1,2 +1,69 @@
 // snack 1
-const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
+const vips = [
+    {
+        guestName: 'Brad Pitt',
+        tableName : 'Tavolo Vip',
+        place: 1
+
+    },
+    {
+        guestName: 'Johnny Depp',
+        tableName : 'Tavolo Vip',
+        place: 2
+
+    },
+    {
+        guestName: 'Lady Gaga',
+        tableName : 'Tavolo Vip',
+        place: 3
+
+    },
+    {
+        guestName: 'Cristiano Ronaldo',
+        tableName : 'Tavolo Vip',
+        place: 4
+
+    },
+    {
+        guestName: 'Georgina Rodriguez',
+        tableName : 'Tavolo Vip',
+        place: 5
+
+    },
+    {
+        guestName: 'Chiara Ferragni',
+        tableName : 'Tavolo Vip',
+        place: 6
+
+    },
+    {
+        guestName: 'Fedez',
+        tableName : 'Tavolo Vip',
+        place: 7
+
+    },
+    {
+        guestName: 'George Cloney',
+        tableName : 'Tavolo Vip',
+        place: 8
+
+    },
+    {
+        guestName: 'Amal Clooney',
+        tableName : 'Tavolo Vip',
+        place: 9
+
+    },
+    {
+        guestName: 'Maneskin',
+        tableName : 'Tavolo Vip',
+        place: 10
+
+    }
+]
+
+const lista = document.getElementById("lista")
+vips.forEach((vip) => );
+
+
+
